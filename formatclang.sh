dab65af7d1dd93763f8685c40ca4589087758219
tcadanklefsen <allen.danklefsen@toyotaconnected.com>
2024-10-07 14:12:30 -0500

    Removing un-needed files

--- formatclang.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-CONFIG_FILE_PATH="/home/devbox/dev/workspace-automation/app/ivi-homescreen-plugins/clang-format"
-find . -iname "*.h" -o -iname "*.cc" | xargs clang-format -i -style=file -fallback-style=none -assume-filename=${CONFIG_FILE_PATH}
